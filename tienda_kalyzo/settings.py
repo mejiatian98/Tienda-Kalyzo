34996539ef2f46d76ee5a357ab6c6cbcebaf02e4
mejiatian98 <halosniper04@gmail.com>
2026-01-04 12:55:53 -0500

    Actualizacion de DB 1

--- tienda_kalyzo/settings.py
+++ tienda_kalyzo/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "app_customers",
     "app_orders",
 
+    'storages',
+
 ]
 
 MIDDLEWARE = [
@@ -142,8 +144,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 
+# ======================
+# AWS S3 CONFIG
+# ======================
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# DROPI_EMAIL = os.getenv("DROPI_EMAIL")
-# DROPI_PASSWORD = os.getenv("DROPI_PASSWORD")
-# DROPI_WHITE_BRAND_ID = os.getenv("DROPI_WHITE_BRAND_ID")
-# DROPI_INTEGRATION_KEY = os.getenv("DROPI_INTEGRATION_KEY")
+# AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+# AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+# AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "us-east-1")
+
+# AWS_QUERYSTRING_AUTH = False
+# AWS_DEFAULT_ACL = None
+
+# MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
