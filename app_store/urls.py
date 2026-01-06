326d05ece7c6d4d1977baca9cd921ac9aea50a2a
mejiatian98 <halosniper04@gmail.com>
2026-01-05 20:48:56 -0500

    Ajustes de Diseño y DB 1

--- app_store/urls.py
+++ app_store/urls.py
@@ -3,14 +3,12 @@ from app_store import views
 
 urlpatterns = [
     path("", views.StoreView.as_view(), name="store_page"),
-    path("categorias/", views.CategoriaView.as_view(), name="categorys"),
-    path("categorias/<slug:slug>/", views.CategoriaProductosView.as_view(),name="category_products"),
-    path("nuevos productos/", views.NewsProductsView.as_view(), name="news_products"),
-    path("buscar/", views.SearchProductsView.as_view(), name="search_products"),
-    path("top ventas/", views.TopSellingProductsView.as_view(), name="top_selling_products"),
-    path("productos oferta/", views.DiscountedProductsView.as_view(), name="discounted_products"),
+    path("productos/categorias/", views.CategoriaView.as_view(), name="categorys"),
+    path("productos/categorias/<slug:slug>/", views.CategoriaProductosView.as_view(),name="category_products"),
+    path("productos/nuevos productos/", views.NewsProductsView.as_view(), name="news_products"),
+    path("productos/buscar/", views.SearchProductsView.as_view(), name="search_products"),
+    path("productos/top ventas/", views.TopSellingProductsView.as_view(), name="top_selling_products"),
+    path("productos/productos oferta/", views.DiscountedProductsView.as_view(), name="discounted_products"),
 
-    
-    
     
 ]
